5b679db7757d6aed69def56c01730118e9e761b0
unknown <vt3630>
2026-02-07 22:03:28 +0530

    demo

--- linux101-killercoda/Lab-02/step-01/verify.sh
+++ linux101-killercoda/Lab-02/step-01/verify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -q "pwd"  && exit 0 || exit 1
+history | grep -q "pwd"  && exit 0 || exit 1
--- linux101-killercoda/Lab-02/step-10/verify.sh
+++ linux101-killercoda/Lab-02/step-10/verify.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 history -a
-sleep 0.5
 grep -q "ls /boot" /root/.bash_history && exit 0 || exit 1
