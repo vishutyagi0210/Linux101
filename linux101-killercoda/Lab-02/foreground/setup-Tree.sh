d57bd88b684489fc3197d0e27999eb017c0c9e19
unknown <vt3630>
2026-02-08 07:49:08 +0530

    lab3 completed

--- linux101-killercoda/Lab-02/foreground/setup-Tree.sh
+++ linux101-killercoda/Lab-02/foreground/setup-Tree.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Try to install tree if missing, suppressing output
+echo "Wait checking the sofware and installing if needed"
 dpkg -s tree &> /dev/null || (sudo apt-get update -qq && sudo apt-get install -y tree -qq)
 export PROMPT_COMMAND='history -a'
 clear
+
