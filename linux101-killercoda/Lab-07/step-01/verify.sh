b5195f61f273dc73dc625edab4c722e7ba809b9e
unknown <vt3630>
2026-02-08 09:08:30 +0530

    doen

--- linux101-killercoda/Lab-07/step-01/verify.sh
+++ linux101-killercoda/Lab-07/step-01/verify.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-if [ -f /home/ubuntu/welcome.txt ] && grep -q 'Hello Linux' /home/ubuntu/welcome.txt; then
+# Check current directory OR home directory
+if [ -f "welcome.txt" ] && grep -q "Hello Linux" "welcome.txt"; then
+    exit 0
+elif [ -f "$HOME/welcome.txt" ] && grep -q "Hello Linux" "$HOME/welcome.txt"; then
+    exit 0
+elif [ -f "/home/ubuntu/welcome.txt" ] && grep -q "Hello Linux" "/home/ubuntu/welcome.txt"; then
     exit 0
 else
     exit 1
